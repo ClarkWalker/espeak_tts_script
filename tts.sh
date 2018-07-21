8cc8f7d7ea99bb7ad85a1270de29b00c8762e9f8
Clark Walker <303walker@gmail.com>
2018-07-21 08:38:27 -0600

    added state function to return a string value to speak and then wipe the xsel value this makes the flow of useing the script a bit more seamless

--- tts.sh
+++ tts.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # this is the smallest and most useful script I have ever written
 killall espeak
-xsel | espeak --stdin
-killall espeak
-xsel -c
+function state {
+	echo $xsel
+	xsel -c
+}
+state | espeak --stdin
+# killall espeak
