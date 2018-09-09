c64c8c824b6261afe056c4e78eebff632d2de103
Clark Walker <303walker@gmail.com>
2018-09-09 13:28:39 -0600

    edited comment

--- tts.sh
+++ tts.sh
@@ -2,5 +2,5 @@
 if pidof espeak > 0; then # is espeak running?
 	killall espeak # stops espeak
 else
-	xsel | espeak --stdin # passes text to espeak
+	xsel | espeak --stdin # pipes selected text to espeak
 fi
