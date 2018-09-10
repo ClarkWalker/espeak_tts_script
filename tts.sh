056a2c1783ed16acba41d53f2b82406a249a0b35
Clark Walker <303walker@gmail.com>
2018-09-10 09:48:55 -0600

    just some comments

--- tts.sh
+++ tts.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if pidof espeak > 0; then # is espeak running?
+if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
 	killall espeak # stops espeak
 else
-	xsel | espeak --stdin # pipes selected text to espeak
+	xsel | espeak --stdin # pipes selected text to espeak # how to make stateful?
 fi
