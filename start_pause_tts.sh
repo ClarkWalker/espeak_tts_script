b6784eea86410e7ffd4dde70b9ff6eb1b4a57d19
Clark Walker <303walker@gmail.com>
2019-06-13 21:31:35 -0600

    nuff sed

--- start_pause_tts.sh
+++ start_pause_tts.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 SPEED=300 # Default speed is 175
 VOLUME=100 # Default Volume is 100
+PAUSE_STATE=$(<pause_state.txt)
+PAUSED="paused"
 
-if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
-	killall espeak # stops espeak
-else
-	xsel | espeak --stdin -a $VOLUME -s $SPEED # pipes selected text to espeak # how to make stateful?
+espeak "$PAUSE_STATE"
+
+if [ cat pause_state.txt == "$PAUSED" ]; then
+    xsel | espeak --stdin -a $VOLUME -s $SPEED # pipes selected text to espeak
 fi
+# if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
+
+#     ESPEAK_PID=$(pidof espeak)
+# 	  kill -STOP $ESPEAK_PID # stops espeak
+# else
+# 	xsel | espeak --stdin -a $VOLUME -s $SPEED # pipes selected text to espeak # how to make stateful?
+# fi
--- start_stop_tts.sh
+++ start_stop_tts.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-SPEED=300 # Default speed is 175
+SPEED=350 # Default speed is 175
 VOLUME=100 # Default Volume is 100
 
 if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
-	killall espeak # stops espeak
+	killall -s KILL espeak # stops espeak
 else
 	xsel | espeak --stdin -a $VOLUME -s $SPEED # pipes selected text to espeak # how to make stateful?
 fi
