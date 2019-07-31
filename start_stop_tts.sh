0882cd22f30153ce2b883e2964906f93228a5be4
Clark Walker <303walker@gmail.com>
2019-07-31 09:18:32 -0600

    various changes

--- start_stop_tts.sh
+++ start_stop_tts.sh
@@ -5,5 +5,6 @@ VOLUME=100 # Default Volume is 100
 if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
 	killall -s KILL espeak # stops espeak
 else
-	xsel | espeak --stdin -a $VOLUME -s $SPEED # pipes selected text to espeak # how to make stateful?
+	#xsel | espeak --stdin -a $VOLUME -s $SPEED # this is the old line, for some reason the last letter is omitted this way
+	espeak -a $VOLUME -s $SPEED "$(xsel) ..." # this method prevents the last letter from being omitted from espeak, that's what the ... is for
 fi
