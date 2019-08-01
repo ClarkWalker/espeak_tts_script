d6dcc9872c19263c39c52d13bc48d52b37b94ef1
Clark Walker <303walker@gmail.com>
2019-07-31 21:06:34 -0600

    finished

--- tts.sh
+++ tts.sh
@@ -6,5 +6,5 @@ if pidof espeak > 0; then  # Check if espeak is running
     killall -s KILL espeak 
 else
     #xsel | espeak --stdin -a $VOLUME -s $SPEED # this is the old line, for some reason the last letter is omitted this way
-    espeak -a $VOLUME -s $SPEED ". $(xsel) ." # this method prevents the last letter from being omitted from espeak, that's what the dots are for
+    espeak -a $VOLUME -s $SPEED ". $(xsel) ." # the dots prevent the first or last phonemes from being omitted
 fi
