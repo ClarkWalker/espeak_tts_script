d6b27a6df76dbc1210f171814daa3e35e28a6c36
Clark Walker <303walker@gmail.com>
2019-07-31 21:01:21 -0600

    what is going on here

--- tts.sh
+++ tts.sh
@@ -2,12 +2,11 @@
 SPEED=350 # Default speed is 175
 VOLUME=100 # Default Volume is 100
 
-if pidof espeak > 0; then # is espeak running? # does this cover edge cases?
-	killall -s KILL espeak # stops espeak
+if pgrep espeak >/dev/null 2>&1; then # is espeak running? # does this cover edge cases?
+    killall -s KILL espeak # stops espeak
 else
-	#xsel | espeak --stdin -a $VOLUME -s $SPEED # this is the old line, for some reason the last letter is omitted this way
-	espeak -a $VOLUME -s $SPEED "... $(xsel) ..." # this method prevents the last letter from being omitted from espeak, that's what the ... is for
+    #xsel | espeak --stdin -a $VOLUME -s $SPEED # this is the old line, for some reason the last letter is omitted this way
+    espeak -a $VOLUME -s $SPEED "... $(xsel) ..." # this method prevents the last letter from being omitted from espeak, that's what the ... is for
 fi
 
 # if pidof espeak > 0; then killall -s KILL espeak; else espeak -a 100 -s 350 "... $(xsel) ...";fi
-
